5863fcfdfc5b63bcd5e8184eaf22c9fe6c782687
Milosz463 <miloszdawiec6@gmail.com>
2025-01-09 08:54:59 +0100

    Create GraKomp.cs

---
+++ GraKomp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace WpfApp2
+{
+    public class GraKomp
+    {
+       public string Nazwa {  get; set; }
+       public string Gatunek { get; set; }
+       public int Wiek {  get; set; }
+       public bool Multiplayer { get; set; }
+
+        public GraKomp(string nazwa, string gatunek, int wiek, bool multiplayer)
+        {
+            Nazwa = nazwa;
+            Gatunek = gatunek;
+            Wiek = wiek;
+            Multiplayer = multiplayer;
+        }
+    }
+}
