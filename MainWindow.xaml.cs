e8a03230a0beda4a3774e57d738267fffe36ee9b
Milosz463 <miloszdawiec6@gmail.com>
2025-01-09 08:55:44 +0100

    Create MainWindow.xaml.cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       public List<GraKomp> ListaGier { get; set; } 
+        public List<string> gatunki { get; set; } = new List<string>() { "rpg", "zrecznosciowa", "fantasy", "FPS" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            KolumnaGatuenk.ItemsSource =gatunki;
+
+            wypelnijDane();
+
+            DataContext = this;
+        }
+
+        public void wypelnijDane()
+        {
+            ListaGier=new List<GraKomp>();
+            ListaGier.Add(new GraKomp("CS2", "FPS", 18, true));
+            ListaGier.Add(new GraKomp("Fortnie", "FPS", 12, true));
+            ListaGier.Add(new GraKomp("Cyberpunk", "rpg", 18, false));
+            ListaGier.Add(new GraKomp("Tetris", "zrecznosciowa", 6, false));
+            ListaGier.Add(new GraKomp("LoL", "fantasy", 13, true));
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    }
+}
